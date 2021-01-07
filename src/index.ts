5bffe5ca2b0a4e567573579eb1ce6f77bf776bc7
shubhsk88 <shubham88singh@gmail.com>
2021-01-07 22:14:37 +0530

    Adding the middlewares

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import express from 'express'
+import cors from 'cors'
+import morgan from 'morgan'
+import helmet from 'helmet'
 import {ApolloServer} from 'apollo-server-express'
 import dotenv from 'dotenv'
 import schema from './schema'
@@ -15,6 +18,9 @@ const server = new ApolloServer({
   },
 })
 const app = express()
-server.applyMiddleware({app})
+app.use(cors())
+app.use(helmet())
+app.use(morgan('dev'))
+server.applyMiddleware({app, cors: false})
 
 app.listen(port, () => console.log(`Server running on ${port}  🚀 `))
