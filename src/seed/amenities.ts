aac3bf4ccfa7c5046b9c971f7011508450e338f9
shubhsk88 <shubham88singh@gmail.com>
2021-01-12 04:54:02 +0530

    Creating the seed files

---
+++ src/seed/amenities.ts
@@ -0,0 +1,45 @@
+import {PrismaClient} from '@prisma/client'
+
+const prisma = new PrismaClient()
+const amenities = [
+  'Kitchen',
+  'Shampoo',
+  'Heating',
+  'Air conditioning',
+  'Washing machine',
+  'Dryer',
+  'Wifi',
+  'Breakfast',
+  'Indoor fireplace',
+  'Hangers',
+  'Iron',
+  'Hair dryer',
+  'Dedicated workspace',
+  'TV',
+  'Cot',
+  'High chair',
+  'Self check-in',
+  'Smoke alarm',
+  'Carbon monoxide alarm',
+  'Private bathroom',
+  'Piano',
+  'Beachfront',
+  'Waterfront',
+  'Ski-in/ski-out',
+]
+
+const createAmenities = async (amenity: string) => {
+  return await prisma.amenity.create({
+    data: {
+      name: amenity,
+    },
+  })
+}
+const runLoop = async () => {
+  for (let amenity of amenities) {
+    const data = await createAmenities(amenity)
+    console.log(data)
+  }
+}
+
+runLoop()
