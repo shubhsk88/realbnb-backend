ddbc43111116901460b6d12878c55408700d7a9e
marcebdev <20135668+marcebdev@users.noreply.github.com>
2021-01-11 16:39:43 -0500

    fix address not included in user context

--- src/utils/decodeUser.ts
+++ src/utils/decodeUser.ts
@@ -2,7 +2,7 @@ import {prisma} from '../index'
 
 export const decodeUser = async (id: string) => {
   try {
-    return await prisma.user.findUnique({where: {id}})
+    return await prisma.user.findUnique({where: {id}, include: { address: true }})
   } catch (error) {
     console.log('Failed go find decoded user')
     return undefined
