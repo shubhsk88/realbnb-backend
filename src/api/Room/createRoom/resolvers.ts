c8af78fd3dfec0293b7b714a3b4c20459f4da266
marcebdev <20135668+marcebdev@users.noreply.github.com>
2021-01-12 16:13:21 -0500

    added initial room seed implementation

--- src/api/Room/createRoom/resolvers.ts
+++ src/api/Room/createRoom/resolvers.ts
@@ -1,9 +1,11 @@
-import { Resolvers } from "@/types/generated";
+/*
+import {Resolvers} from '@/types/generated'
 
-const resolvers : Resolvers = {
+const resolvers: Resolvers = {
   Mutation: {
     async createRoom(_, args, context) {
-      if (!context.user || !context.user.isVerified) return {ok: false, error: 'Unknown error occurred'}
+      if (!context.user || !context.user.isVerified)
+        return {ok: false, error: 'Unknown error occurred'}
 
       // const rooms = context.user.rooms
       await context.prisma.user.update({
@@ -13,23 +15,28 @@ const resolvers : Resolvers = {
         data: {
           rooms: {
             // FIXME: join user.rooms, proper address & roomType definition
-            create: [{ ...args,
-              address: {
-                create: {
-                  city: '',
-                  country: '',
-                  address: ''
-                }
+            create: [
+              {
+                ...args,
+                address: {
+                  create: {
+                    city: '',
+                    country: '',
+                    address: '',
+                  },
+                },
+                roomType: {
+                  create: {name: ''},
+                },
               },
-              roomType: {
-                create: { name: '' }
-              }
-            }]
-        }
-      }})
-      return { ok: true }
-    }
-  }
+            ],
+          },
+        },
+      })
+      return {ok: true}
+    },
+  },
 }
 
 export default resolvers
+*/
