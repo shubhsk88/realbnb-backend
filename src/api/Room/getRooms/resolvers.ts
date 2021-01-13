5a089f2ef5870c30c9eaf42a7732455a3a021f31
marcebdev <20135668+marcebdev@users.noreply.github.com>
2021-01-13 17:26:01 -0500

    added rooms query

---
+++ src/api/Room/getRooms/resolvers.ts
@@ -0,0 +1,22 @@
+import {Resolvers} from '@/types/generated'
+
+const resolvers: Resolvers = {
+  Query: {
+    getRooms: async (_, __, context) => {
+      try {
+        const rooms = await context.prisma.room.findMany()
+        return {
+          ok: true,
+          rooms,
+        }
+      } catch (error) {
+        return {
+          ok: false,
+          error: error.message,
+        }
+      }
+    },
+  },
+}
+
+export default resolvers
